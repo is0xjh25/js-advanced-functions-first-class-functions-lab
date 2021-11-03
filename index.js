063b868456f24cbb8bf295438d2779889df237b3
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2021-11-03 12:31:06 +0800

    Update: Test passed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(arr) {
+	return [arr[0], arr[1]];
+}
+
+function returnLastTwoDrivers(arr) {
+	return [arr[arr.length - 2], arr[arr.length - 1]];
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function  createFareMultiplier(x) {
+	return function() {
+		return 5 * x;
+	}
+}
+
+function fareDoubler(fn) {
+	return fn * 2;
+}
+
+function fareTripler(fn) {
+	return fn * 3;
+}
+
+function selectDifferentDrivers(arr, fn){
+	return fn(arr);
+}
